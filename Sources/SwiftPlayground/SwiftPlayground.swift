f97a4af14bc2e31ae1b92267df0fb91e0fb3e283
oc-val-meza-26 <val.meza@student.onslow.school.nz>
2026-05-27 02:29:40 +0000

    May 27, 2026, 2:29 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,13 +7,13 @@ struct SwiftPlayground {
     static func main() {
 
 print("Price per item: $19.99")
-let pricePerItem = 19.99
+let pricePerItem = Double 19.99
 
 print("Quantity: 3.0")
 let quantity = 3.0
 
 print("Subtotal: $\(19.99 * 3.0)")
-if pricePerItem 
+if subtotal == pricePerItem * quantity
 
 print("Tax (15%): $\(19.99 * 3.0 * 0.15)")
 print("Total: $\(19.99 * 3.0 * 1.15)")
