940fcf8fb87740ce2f470c7343e2bfb3e62e80ae
oc-val-meza-26 <val.meza@student.onslow.school.nz>
2026-05-27 02:37:04 +0000

    May 27, 2026, 2:37 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -22,5 +22,12 @@ let tax: Double = subtotal * taxRate
 print("Total: $\(19.99 * 3.0 * 1.15)")
 let total: Double = subtotal * taxRate
 
+//task
+
+print("The length of the rectangle is \(12.5) units.")
+print("The width of the rectangle is \(8) units.")
+print("The area of the rectangle is \(12.5 * 8.0) square units.")
+print("The perimeter of the rectangle is \(2 * (length + width)) units.")
+
     }
 }
