fbabf9fa56b4064a79e5978838064214e0202693
oc-val-meza-26 <val.meza@student.onslow.school.nz>
2026-06-01 22:27:37 +0000

    Jun 1, 2026, 10:27 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,9 +5,18 @@ import Foundation
 @main
 struct SwiftPlayground {
     static func main() {
+        
+//task three
+print ("what is ur name?")
+let firstName = readLine()!
+print ("name the subject you take")
+let subject = readLine()!
+print (" what score did you get for \(subject)?")
+userInput = readLine()!
+let score = Double(userInput)!
 
-
-
+//print
+print (" \(firstName) got \(score) in \(subject)")
 
 
 
