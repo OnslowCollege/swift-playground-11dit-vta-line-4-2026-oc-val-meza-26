345753e0eb63d4fe88e2a86ff6cab77382870ed9
oc-val-meza-26 <val.meza@student.onslow.school.nz>
2026-06-01 22:10:05 +0000

    Jun 1, 2026, 10:10 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,30 +6,7 @@ import Foundation
 struct SwiftPlayground {
     static func main() {
 
-print("Price per item: $19.99")
-let pricePerItem: Double = 19.99
+//cost of items purchased 
 
-print("Quantity: 3.0")
-let quantity: Double = 0.3
-
-print("Subtotal: $\(19.99 * 3.0)")
-let subtotal: Double = pricePerItem * quantity
-
-print("Tax (15%): $\(19.99 * 3.0 * 0.15)")
-let taxRate: Double = 1.15
-let tax: Double = subtotal * taxRate
-
-print("Total: $\(19.99 * 3.0 * 1.15)")
-let total: Double = subtotal * taxRate
-
-//task
-let width: Double = 8
-let length: Double = 12.5
-let area: Double = length * width
-let perimeter: Double = 2 * (length + width)
-print("The length of the rectangle is \(12.5) units.")
-print("The width of the rectangle is \(8) units.")
-print("The area of the rectangle is \(area) square units.")
-print("The perimeter of the rectangle is \(perimeter) units.")
     }
 }
